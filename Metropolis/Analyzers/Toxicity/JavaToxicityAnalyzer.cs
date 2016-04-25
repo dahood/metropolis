f0102be25ab1252cd138e162abeb73c9dff86c2b
Jonathan McCracken <jon@nexicon.ca>
2016-04-25 13:15:17 -0600

    starting to code Java toxicity

--- Metropolis/Analyzers/Toxicity/JavaToxicityAnalyzer.cs
+++ Metropolis/Analyzers/Toxicity/JavaToxicityAnalyzer.cs
@@ -1,5 +1,4 @@
-﻿using Metropolis.Analyzers.Toxicity;
-using Metropolis.Domain;
+﻿using Metropolis.Domain;
 
 namespace Metropolis.Analyzers.Toxicity
 {
@@ -7,9 +6,6 @@ namespace Metropolis.Analyzers.Toxicity
     {
         //class level thresholds
         private const int ThresholdLinesOfCode = 500;
-        private const int ThresholdClassCoupling = 30;
-        private const int ThresholdDepthOfInheritance = 3;
-        private const int DepthOfInheritanceFactor = 5; // Increase the effect on toxicity when this rule is violated
         private const int ThresholdNumberOfMethods = 20;
         // method level thresholds
         private const int ThresholdMethodLength = 30;
@@ -17,27 +13,23 @@ namespace Metropolis.Analyzers.Toxicity
 
         public override ToxicityScore CalculateToxicity(Class classToScore)
         {
-            //TODO: replace with CheckStyle data sources...there are more than C#
-
             // Class Level Toxicity
             var linesOfCode = ComputeToxicity(classToScore.LinesOfCode, ThresholdLinesOfCode);
-            var classCoupling = ComputeToxicity(classToScore.ClassCoupling, ThresholdClassCoupling);
-            var depthOfInheritance = ComputeToxicity(classToScore.DepthOfInheritance, ThresholdDepthOfInheritance);
             var numberOfMethods = ComputeToxicity(classToScore.Members.Count, ThresholdNumberOfMethods);
 
             double cyclomaticComplexity = 0;
+            double methodLength = 0;
             // Method Level Toxicity
             foreach (var method in classToScore.Members)
             {
                 cyclomaticComplexity += ComputeToxicity(method.CylomaticComplexity, thresholdCyclomaticComplexity);
-                linesOfCode += ComputeToxicity(method.LinesOfCode, ThresholdMethodLength);
+                methodLength += ComputeToxicity(method.LinesOfCode, ThresholdMethodLength);
             }
 
             // Rationalize
             var score = new ToxicityScore();
             score.LinesOfCode = Rationalize(linesOfCode);
-            score.ClassCoupling = Rationalize(classCoupling);
-            score.DepthOfInheritance = Rationalize(depthOfInheritance * DepthOfInheritanceFactor);
+            score.MethodLength = Rationalize(methodLength);
             score.NumberOfMethods = Rationalize(numberOfMethods);
             score.CyclomaticComplexity = Rationalize(cyclomaticComplexity);
 
