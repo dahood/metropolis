262abba6f5944b6cfb57787cc617536e3ec5492e
Jonathan McCracken <jon@nexicon.ca>
2016-05-07 06:39:35 -0600

    defining cli console entry point

--- src/Metropolis.Cli.Console/Program.cs
+++ src/Metropolis.Cli.Console/Program.cs
@@ -1,11 +1,22 @@
 ﻿namespace Metropolis.Cli.Console
-{class Program
+{
+    /// <summary>
+    /// Command line interface to hook up to build agents like Teamcity
+    /// </summary>
+    class Metropolis
     {
         static void Main(string[] args)
         {
-            System.Console.Write("hi");
-            System.Console.Write("there");
-            
+            if (args.Length == 0)
+            { 
+            System.Console.Write("hi there I'm metropolis");
+            System.Console.Write("Enter Directory:");
+            var directory = System.Console.ReadLine();
+            System.Console.Write("Enter Source Type (java,javascript,csharp, default:csharp):");
+            var sourceType = System.Console.ReadLine();
+            }
+            System.Console.Write("Metropolis v0.0.1 - Command Usage: metropolis.exe -d c:\dev\metropolis -s csharp");
+            //TODO: hook this into Metropolis.API
         }
     }
 }
