13820a1f308df7b7629d185f32183be4236717e2
Jonathan McCracken <jon@nexicon.ca>
2016-05-11 11:30:04 -0600

    added compile task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,11 @@ var gulp = require('gulp');
 var shell = require('gulp-shell');
 var nunit = require('gulp-nunit-runner');
 
+gulp.task('compile', shell.task([
+  'echo compiling',
+  '\"C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe\" Metropolis.sln /p:OutDir=%CD%\\build /maxcpucount:8'
+]))
+
 gulp.task('default', function() {
   // place code for your default task here
 });
