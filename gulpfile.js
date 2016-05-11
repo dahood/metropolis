2234fdb352855bdff89153190684598c4cbc84ff
Jonathan McCracken <jon@nexicon.ca>
2016-05-11 12:20:06 -0600

    gulping it up

--- gulpfile.js
+++ gulpfile.js
@@ -1,12 +1,25 @@
+// Gulp Modules
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 var nunit = require('gulp-nunit-runner');
 
+// Gulp Variables
+var buildPath = '%CD%\\build';
+var maxThreads = 8;
+
+// Gulp Tasks
+
 gulp.task('compile', shell.task([
-  'echo compiling',
-  '\"C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe\" Metropolis.sln /p:OutDir=%CD%\\build /maxcpucount:8'
+  '\"C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe\" Metropolis.sln /p:OutDir=' + buildPath + ' /maxcpucount:' + maxThreads
 ]))
 
-gulp.task('default', function() {
-  // place code for your default task here
+gulp.task('test', function () {
+    return gulp.src(['build\\*.Test.dll'], {read: false})
+        .pipe(nunit({
+            executable: 'packages\\NUnit.ConsoleRunner.3.2.1\\tools\\nunit3-console.exe',
+            result: 'build\\TestResults.xml',
+            workers: maxThreads
+        }));
 });
+
+gulp.task('default', ['compile', 'test']);
