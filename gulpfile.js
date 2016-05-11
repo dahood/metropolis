86f0216cddfd3a66bc0ab9576c84d14e718c61b4
Jonathan McCracken <jon@nexicon.ca>
2016-05-11 14:41:43 -0600

    adding dist target for gulp - trying to remove msi installer

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 // Gulp Modules
+var del = require('del');
 var gulp = require('gulp');
 var shell = require('gulp-shell');
 var nunit = require('gulp-nunit-runner');
@@ -8,6 +9,10 @@ var buildPath = '%CD%\\build';
 var maxThreads = 8;
 
 // Gulp Tasks
+
+gulp.task('clean', function () {
+  return del(['build/**/*','dist/**/*']);
+});
 
 gulp.task('compile', shell.task([
   '\"C:\\Program Files (x86)\\MSBuild\\14.0\\Bin\\MSBuild.exe\" Metropolis.sln /p:OutDir=' + buildPath + ' /maxcpucount:' + maxThreads
@@ -24,6 +29,12 @@ gulp.task('test', function () {
             workers: maxThreads,
             executable: 'packages\\NUnit.ConsoleRunner.3.2.1\\tools\\nunit3-console.exe'
         }));
+});
+
+gulp.task('dist', ['clean', 'compile'], function(){
+	return gulp.src(['build\\*.dll', 'build\\*.exe', 'build\\*.config', 
+			'!build\\Metropolis.Test.dll','!build\\FluentAssertions.Core.dll', '!build\\FluentAssertions.dll', '!build\\nunit.framework.dll'])
+		.pipe(gulp.dest('dist'));
 });
 
 gulp.task('default', ['compile', 'test']);
