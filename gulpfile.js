2395604fe22fd3bc51dc7ce9526d113796bbb298
Jonathan McCracken <jon@nexicon.ca>
2016-05-11 11:00:45 -0600

    more gulping

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,6 @@
 var gulp = require('gulp');
+var shell = require('gulp-shell');
+var nunit = require('gulp-nunit-runner');
 
 gulp.task('default', function() {
   // place code for your default task here
