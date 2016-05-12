f370051bb5cb676d1c5bf228dff2194386debf8b
Jonathan McCracken <jon@nexicon.ca>
2016-05-11 20:31:37 -0600

    another fix because package name

--- create-shortcut.ps1
+++ create-shortcut.ps1
@@ -1,6 +1,6 @@
 $shell = New-Object -ComObject WScript.Shell
 $desktop = [System.Environment]::GetFolderPath('Desktop')
 $shortcut = $shell.CreateShortcut("$desktop\Metropolis.lnk")
-$shortcut.TargetPath = $env:APPDATA + "\npm\node_modules\metropolis\dist\metropolis.exe"
-$shortcut.IconLocation = $env:APPDATA + "\npm\node_modules\metropolis\dist\metropolis.exe,0"
+$shortcut.TargetPath = $env:APPDATA + "\npm\node_modules\metropolis-core\dist\metropolis.exe"
+$shortcut.IconLocation = $env:APPDATA + "\npm\node_modules\metropolis-core\dist\metropolis.exe,0"
 $shortcut.Save()
