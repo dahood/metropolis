e1ffc2fe9446f87dcd9461c6c754aa319689fe96
Jonathan McCracken <jon@nexicon.ca>
2016-05-11 19:22:28 -0600

    got npm working for install and uninstall...still need to remove installer

--- create-shortcut.ps1
+++ create-shortcut.ps1
@@ -1,6 +1,6 @@
 $shell = New-Object -ComObject WScript.Shell
 $desktop = [System.Environment]::GetFolderPath('Desktop')
 $shortcut = $shell.CreateShortcut("$desktop\Metropolis.lnk")
-$shortcut.TargetPath = "~AppData\Roaming\npm\metropolis\dist\metropolis.exe"
-$shortcut.IconLocation = "~AppData\Roaming\npm\metropolis\dist\metropolis.exe,0"
+$shortcut.TargetPath = $env:APPDATA + "\npm\node_modules\metropolis\dist\metropolis.exe"
+$shortcut.IconLocation = $env:APPDATA + "\npm\node_modules\metropolis\dist\metropolis.exe,0"
 $shortcut.Save()
