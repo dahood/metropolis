f2aef1a791d443028b7b7235c4be359c20240e2d
Jonathan McCracken <jon@nexicon.ca>
2016-05-13 11:40:09 -0600

    renaming install scripts

--- metro-install.ps1
+++ metro-install.ps1
@@ -1,6 +1,10 @@
+# create shortcut for desktop
+
 $shell = New-Object -ComObject WScript.Shell
 $desktop = [System.Environment]::GetFolderPath('Desktop')
 $shortcut = $shell.CreateShortcut("$desktop\Metropolis.lnk")
 $shortcut.TargetPath = $env:APPDATA + "\npm\node_modules\metropolis-core\dist\metropolis.exe"
 $shortcut.IconLocation = $env:APPDATA + "\npm\node_modules\metropolis-core\dist\metropolis.exe,0"
 $shortcut.Save()
+
+# add classpath for jar file
