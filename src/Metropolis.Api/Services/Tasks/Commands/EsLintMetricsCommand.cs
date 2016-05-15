2dede88cb3c8334d0a58fa78f54fe74695c2cd7b
Greg Cook <gcook1@shaw.ca>
2016-05-15 17:15:42 -0600

    allow for Eslint to run without the need for an ignore file

--- src/Metropolis.Api/Services/Tasks/Commands/EsLintMetricsCommand.cs
+++ src/Metropolis.Api/Services/Tasks/Commands/EsLintMetricsCommand.cs
@@ -7,7 +7,8 @@ namespace Metropolis.Api.Services.Tasks.Commands
 {
     public class EsLintMetricsCommand : BaseMetricsCommand
     {
-        private const string EsLintCommand = @"eslint  -c '{0}\.eslintrc.json' '{1}\**'  -o '{2}' -f checkstyle --ignore-path '{3}'";
+        private const string EsLintCommand = @"eslint  -c '{0}\.eslintrc.json' '{1}\**'  -o '{2}' -f checkstyle ";
+        private const string IgnorePathPart = "  --ignore - path '{0}'";
 
         public override string MetricsType => "Eslint";
         public override string Extension => ".xml";
@@ -15,7 +16,9 @@ namespace Metropolis.Api.Services.Tasks.Commands
         public override IEnumerable<MetricsResult> Run(MetricsCommandArguments args)
         {
             var result = new MetricsResult {ParseType = ParseType.EsLint, MetricsFile = GetMetricsOutoutFile(args)};
-            var cmd = EsLintCommand.FormatWith(Environment.CurrentDirectory, args.SourceDirectory, result.MetricsFile, args.IgnorePath);
+            var cmd = EsLintCommand.FormatWith(Environment.CurrentDirectory, args.SourceDirectory, result.MetricsFile);
+            if (args.IgnorePath.IsNotEmpty())
+                string.Concat(cmd, IgnorePathPart.FormatWith(args.IgnorePath));
             SaveAndExecuteCommand(args, cmd);
             return new[] {result};
         }
