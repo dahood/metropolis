28ac2a9cbfc06775ee68b4816a93192fe981511a
Jonathan McCracken <jon@nexicon.ca>
2016-05-21 10:05:41 -0600

    setting working directory to dist

--- metro-install.ps1
+++ metro-install.ps1
@@ -4,5 +4,6 @@ $shell = New-Object -ComObject WScript.Shell
 $desktop = [System.Environment]::GetFolderPath('Desktop')
 $shortcut = $shell.CreateShortcut("$desktop\Metropolis.lnk")
 $shortcut.TargetPath = $env:APPDATA + "\npm\node_modules\metropolis\dist\metropolis.exe"
+$shortcut.WorkingDirectory = $env:APPDATA + "\npm\node_modules\metropolis\dist\"
 $shortcut.IconLocation = $env:APPDATA + "\npm\node_modules\metropolis\dist\metropolis.exe,0"
 $shortcut.Save()
