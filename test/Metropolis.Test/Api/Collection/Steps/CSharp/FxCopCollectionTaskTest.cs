e4f397be556b584e1e6532d82e2ba0fd9ca0b8ca
Greg Cook <gcook1@shaw.ca>
2016-05-22 12:16:59 -0600

    VisualStudioCollectionSteps unit tests

---
+++ test/Metropolis.Test/Api/Collection/Steps/CSharp/FxCopCollectionTaskTest.cs
@@ -0,0 +1,58 @@
+﻿using Metropolis.Api.Collection.PowerShell;
+using Metropolis.Api.Collection.Steps.CSharp;
+using Metropolis.Api.Utilities;
+using Metropolis.Common.Extensions;
+using Metropolis.Common.Models;
+using Metropolis.Test.Api.Services;
+using Metropolis.Test.Utilities;
+using Moq;
+using NUnit.Framework;
+
+namespace Metropolis.Test.Api.Collection.Steps.CSharp
+{
+    [TestFixture]
+    public class FxCopCollectionTaskTest : StrictMockBaseTest
+    {
+        private FxCopCollectionTask task;
+        private Mock<IRunPowerShell> powerShell;
+        private Mock<IFileSystem> fileSystem;
+        private readonly MetricsCommandArguments args = new MetricsCommandArguments
+                                                            {
+                                                                MetricsOutputDirectory = @"c:\metrics",
+                                                                ProjectName = "test",
+                                                                RepositorySourceType = RepositorySourceType.FxCop,
+                                                                SourceDirectory = @"c:\source"
+                                                            };
+
+        private const string DllName = "mydll.dll";
+        private readonly MetricsResult expectedResult = new MetricsResult {
+                                                                ParseType = ParseType.FxCop,
+                                                                MetricsFile = @"c:\metrics\test_mydll_metrics.xml"
+                                                        };
+
+
+        [SetUp]
+        public void SetUp()
+        {
+            powerShell = CreateMock<IRunPowerShell>();
+            fileSystem = CreateMock<IFileSystem>();
+
+            task = new FxCopCollectionTask(powerShell.Object, fileSystem.Object);
+        }
+
+        [Test]
+        public void CanRunCOllectionTask()
+        {
+            var expectedCommand = FxCopCollectionTask.CommandTemplate.FormatWith(DllName, expectedResult.MetricsFile);
+            fileSystem.Setup(x => x.GetFileName(DllName)).Returns("mydll");
+            powerShell.Setup(x => x.Invoke(expectedCommand, false));
+            var result = task.Run(args, DllName);
+
+            Validate.Begin()
+                    .IsNotNull(result, "Result").Check()
+                    .IsEqual(result.MetricsFile, expectedResult.MetricsFile, "MetricsFile")
+                    .IsEqual(result.ParseType, expectedResult.ParseType, "ParseType")
+                    .Check();
+        }
+    }
+}
---
+++ test/Metropolis.Test/Api/Collection/Steps/CSharp/VisualStudioCollectionStepsTest.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq;
+using FluentAssertions;
+using Metropolis.Api.Collection.Steps.CSharp;
+using Metropolis.Common.Models;
+using Metropolis.Test.Api.Services;
+using Moq;
+using NUnit.Framework;
+
+namespace Metropolis.Test.Api.Collection.Steps.CSharp
+{
+    [TestFixture]
+    public class VisualStudioCollectionStepsTest : StrictMockBaseTest
+    {
+        private VisualStudioCollectionSteps step;
+        private Mock<ICollectAssemblies> assemblyCollection;
+        private Mock<IFxCopCollectionTask> metricsTask;
+
+        [SetUp]
+        public void SetUp()
+        {
+            assemblyCollection = CreateMock<ICollectAssemblies>();
+            metricsTask = CreateMock<IFxCopCollectionTask>();
+            step = new VisualStudioCollectionSteps(assemblyCollection.Object, metricsTask.Object);
+        }
+
+        [Test]
+        public void CanCollectFxCopMetrics()
+        {
+            var args =new MetricsCommandArguments();
+            const string assembly = "myAssembly.dll";
+
+            assemblyCollection.Setup(x => x.GatherAssemblies(args)).Returns(new[] {assembly});
+            metricsTask.Setup(x => x.Run(args, assembly)).Returns(new MetricsResult());
+
+            var results =  step.Run(args);
+
+            results.Should().NotBeEmpty();
+            results.Count().Should().Be(1);
+        }
+    }
+}
