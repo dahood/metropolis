ff50e56d0e9bcbceb4a9c4e53db5f4dce4e2d00a
Greg Cook <gcook1@shaw.ca>
2016-06-04 08:15:25 -0600

    removed the autocomplete text box as it wasn't functioning properly.

--- test/Metropolis.Test/Properties/AssemblyInfo.cs
+++ test/Metropolis.Test/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Test.Metropolis")]
 [assembly: AssemblyCopyright("Copyright © Jonathan McCracken, Greg Cook, and Richard Hurst 2016")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.0.0.25")]
-[assembly: AssemblyFileVersion("0.0.0.25")]
+[assembly: AssemblyVersion("0.0.0.26")]
+[assembly: AssemblyFileVersion("0.0.0.26")]
