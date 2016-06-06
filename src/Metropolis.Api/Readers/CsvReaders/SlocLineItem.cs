409b86b1394f9c9399f2896d67610fc69ffef32b
Jonathan McCracken <jon@nexicon.ca>
2016-06-06 01:26:53 -0600

    fixing up canvas

---
+++ src/Metropolis.Api/Readers/CsvReaders/SlocLineItem.cs
@@ -0,0 +1,17 @@
+namespace Metropolis.Api.Readers.CsvReaders
+{
+    public class SlocLineItem
+    {
+//        Path,Physical,Source,Comment,Single-line comment, Block comment,Mixed,Empty
+//        angular.js\src\angular.bind.js,12,7,3,3,0,0,2
+        public string Directory { get; set; }
+        public string FileName { get; set; }
+        public int PhysicalLoc { get; set; }
+        public int SourceLoc { get; set; }
+        public int CommentLoc { get; set; }
+        public int SingleLineCommentLoc { get; set; }
+        public int BlockCommentLoc { get; set; }
+        public int MixedLoc { get; set; }
+        public int EmptyLoc { get; set; }
+    }
+}
---
+++ src/Metropolis.Api/Readers/CsvReaders/SlocReader.cs
@@ -0,0 +1,78 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using CsvHelper.Configuration;
+using Metropolis.Api.Domain;
+using Metropolis.Api.Extensions;
+using Metropolis.Api.Readers.CsvReaders.TypeConverters;
+using Metropolis.Api.Readers.CsvReaders.TypeConverters.Sloc;
+
+namespace Metropolis.Api.Readers.CsvReaders
+{
+    public enum FileInclusion
+    {
+        [Description(".js")] Js,
+
+        [Description(".cs")] CSharp,
+
+        [Description(".java")] Java
+    }
+
+    public class SlocReader : CsvInstanceReader<SlocLineItem, SourceLinesOfCodeMap>
+    {
+        public SlocReader(FileInclusion inclusion = FileInclusion.Js) : base(true)
+        {
+            Inclusion = inclusion;
+        }
+
+        public SlocReader() : this(FileInclusion.Js)
+        {
+        }
+
+        private FileInclusion Inclusion { get; }
+
+        protected override CodeBase ParseLines(IEnumerable<SlocLineItem> lines)
+        {
+            var inclusionExtension = Inclusion.GetDescription();
+            var inclusionCodeBagType = MapToCodeBag(Inclusion);
+            var classes = lines.Where(x => x.FileName.EndsWith(inclusionExtension))
+                //TODO: Grab physical path!!!!
+                .Select(each => new Instance(each.FileName, each.Directory, inclusionCodeBagType, string.Empty) {LinesOfCode = each.SourceLoc})
+                .ToList();
+
+            return new CodeBase(new CodeGraph(classes));
+        }
+
+        private CodeBagType MapToCodeBag(FileInclusion inclusion)
+        {
+            switch (inclusion)
+            {
+                case FileInclusion.CSharp:
+                    return CodeBagType.Namespace;
+                case FileInclusion.Java:
+                    return CodeBagType.Package;
+                case FileInclusion.Js:
+                    return CodeBagType.Directory;
+
+                default:
+                    return CodeBagType.Empty;
+            }
+        }
+    }
+
+    public sealed class SourceLinesOfCodeMap : CsvClassMap<SlocLineItem>
+    {
+        public SourceLinesOfCodeMap()
+        {
+            Map(x => x.Directory).Index(0).TypeConverter<SlocDirectoryConverter>();
+            Map(x => x.FileName).Index(0).TypeConverter<SlocFileNameConverter>();
+            Map(x => x.PhysicalLoc).Index(1).TypeConverter<IntTypeConverter>();
+            Map(x => x.SourceLoc).Index(2).TypeConverter<IntTypeConverter>();
+            Map(x => x.CommentLoc).Index(3).TypeConverter<IntTypeConverter>();
+            Map(x => x.SingleLineCommentLoc).Index(4).TypeConverter<IntTypeConverter>();
+            Map(x => x.BlockCommentLoc).Index(5).TypeConverter<IntTypeConverter>();
+            Map(x => x.MixedLoc).Index(6).TypeConverter<IntTypeConverter>();
+            Map(x => x.EmptyLoc).Index(7).TypeConverter<IntTypeConverter>();
+        }
+    }
+}
