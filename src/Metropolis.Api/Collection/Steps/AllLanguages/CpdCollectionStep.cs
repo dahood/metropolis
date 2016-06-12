5821ab1bbe9f5afb5a10d05079d731d58dce5201
Jonathan McCracken <jon@nexicon.ca>
2016-06-11 22:44:31 -0600

    ensure cpd jars are copied as part of build

--- src/Metropolis.Api/Collection/Steps/AllLanguages/CpdCollectionStep.cs
+++ src/Metropolis.Api/Collection/Steps/AllLanguages/CpdCollectionStep.cs
@@ -14,28 +14,27 @@ namespace Metropolis.Api.Collection.Steps.AllLanguages
         /// <summary>
         ///     Thresholds set per language as below, or DEFAULT
         /// </summary>
-        public const int CsharpThreshold = 75;
-
-        public const int EcmaScriptThreshold = 50;
-        public const int JavaThreshold = 50;
-        public const int DefaultThreshold = 75;
+        private const int CsharpThreshold = 75;
+        private const int EcmaScriptThreshold = 75;
+        private const int JavaThreshold = 75;
+        private const int DefaultThreshold = 75;
 
         /// <summary>
         ///     CPD has a token for each language it supports, along with an associated .jar file to cover specifics
         ///     like using statements in C#
         /// </summary>
         private const string CsharpToken = "cs";
-
         private const string EcmaScriptToken = "javascript";
         private const string JavaToken = "java";
         private const string DefaultToken = "java";
 
-        //sample commmand
-        // java -cp {0} net.sourceforge.pmd.cpd.CPD --format csv --language {1} --minimum-tokens {2} --files {3} > {4}
-        //-minimum-tokens 100 --language cs --format csv --files c:\dev\metropolis > results.csv
-
+        //sample commmand  
+        //java -cp "cpd/*" net.sourceforge.pmd.cpd.CPD --format csv --language cs --minimum-tokens 50 --files C:\dev\metropolis\src > cpd-metro.csv
+        //java -cp "cpd/*" net.sourceforge.pmd.cpd.CPD --format csv --language java --minimum-tokens 50 --files C:\Dev\disruptor\src > cpd-disruptor.csv
+        //java -cp "cpd/*" net.sourceforge.pmd.cpd.CPD --format csv --language javascript --minimum-tokens 50 --files C:\Dev\d3\src > cpd-d3.csv
         private const string CpdCommand =
             @"java -cp {0} net.sourceforge.pmd.cpd.CPD --format csv --language {1} --minimum-tokens {2} --files {3} > {4}";
+        private const string ClassPath = @"cpd/*";
 
         public CpdCollectionStep(ParseType parseType) : base(new RunPowerShell())
         {
@@ -49,11 +48,12 @@ namespace Metropolis.Api.Collection.Steps.AllLanguages
         {
             var tokenThreshold = GetThreshold();
             var languageToken = GetLanguageToken();
-            return CpdCommand.FormatWith(LocateBinaries("pmd-*.jar"), languageToken, tokenThreshold, args.SourceDirectory, result.MetricsFile);
+            return CpdCommand.FormatWith(LocateBinaries(ClassPath), languageToken, tokenThreshold, args.SourceDirectory, result.MetricsFile);
         }
 
         private string GetLanguageToken()
         {
+            //TODO this would be better as a Dictionary<ParseType, string>
             switch (ParseType)
             {
                 case ParseType.CpdCsharp:
@@ -69,6 +69,7 @@ namespace Metropolis.Api.Collection.Steps.AllLanguages
 
         private int GetThreshold()
         {
+            //TODO this would be better as a Dictionary<ParseType, int>
             switch (ParseType)
             {
                 case ParseType.CpdCsharp:
