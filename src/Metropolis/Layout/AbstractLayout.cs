3d70e0698a23e46e60d1ff19098863548d4c419e
jon@nexicon.ca <jon@nexicon.ca>
2016-06-15 12:05:38 -0600

    not broken anymore

--- src/Metropolis/Layout/AbstractLayout.cs
+++ src/Metropolis/Layout/AbstractLayout.cs
@@ -101,7 +101,7 @@ namespace Metropolis.Layout
 
         private Model3D CreateCube(double x, double z, int minHeight, int maxHeight, Instance c)
         {
-            var color = HeatMap.GetBrushForToxicity(c.Toxicity);
+            var color = HeatMapBrushFactory.Toxicity(c.Toxicity);
             var scaledHeight = LinearScale.Apply(c.LinesOfCode, minHeight, maxHeight);
 
             var result = CreateCube(x, z, scaledHeight, color);
