d994be035970c043f8ad88a07a5a8d0991d2ae0a
Jonathan McCracken <jon@nexicon.ca>
2016-06-20 20:44:39 -0600

    adding user preference window

---
+++ src/Metropolis/Views/UserPreferences.xaml.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows;
+
+namespace Metropolis.Views
+{
+    /// <summary>
+    ///     Set user preferences like where is FxCop, MSBuild, etc.
+    /// </summary>
+    public partial class UserPreferences
+    {
+        public UserPreferences()
+        {
+            InitializeComponent();
+        }
+
+        private static IWorkspaceProvider WorkSpaceProvider => App.WorkspaceProvider;
+
+        private void SelectFxCopLocation(object sender, RoutedEventArgs e)
+        {
+            //WorkSpaceProvider. = DialogUtils.GetFileName("exe", FxCopBinaryLocationTextBox.Text);
+        }
+    }
+}
