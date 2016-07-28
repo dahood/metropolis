6529801584b6370d429c04ed119afc9430e423c0
Jonathan McCracken <jon@nexicon.ca>
2016-07-28 02:12:20 -0600

    Update ToxicityScore.cs

--- src/Metropolis.Api/Analyzers/Toxicity/ToxicityScore.cs
+++ src/Metropolis.Api/Analyzers/Toxicity/ToxicityScore.cs
@@ -8,9 +8,10 @@
         public double NumberOfMethods { get; set; }
         public double MethodLength { get; set; }
         public double CyclomaticComplexity { get; set; }
+        public double LinesOfCode { get; set; } // Class (Java/C#) or File (ECMA)
 
         // Mostly Common (2 or more)
-        public double LinesOfCode { get; set; } // Class (Java/C#) or File (ECMA)
+
         public double ParameterNumber { get; set; } // C# doesn't use this
         public double NestedIfDepth { get; set; } // Java & ECMA only
         public double MissingDefaultCase { get; set; } // Java & ECMA only
@@ -22,12 +23,12 @@
         //Java
         public double BooleanExpressionComplexity { get; set; }
         public double NestedTryDepth { get; set; }
+        public double ClassFanOutComplexity { get; set; }
         public double AnonInnerLength { get; set; }
         public double ClassDataAbstractionCoupling { get; set; }
         
         // ECMA / Javascript
         public double SwitchNoFallThrough { get; set; }
-        public double ClassFanOutComplexity { get; set; }
         public double NumberOfParameters { get; set; }
     }
 }
