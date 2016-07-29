ba5c1ce753f48e4416812ffdea9c4b85e7efff75
Jonathan McCracken <jon@nexicon.ca>
2016-07-28 20:45:32 -0600

    fixing missing using statements

--- src/Metropolis/Utilities/DialogUtils.cs
+++ src/Metropolis/Utilities/DialogUtils.cs
@@ -6,18 +6,20 @@ namespace Metropolis.Utilities
     {
         public static string GetSourceDirectory(string type, string initialDirectory)
         {
-            var dialog = new FolderBrowserDialog { Description = $"Locate {type} Source Directory", SelectedPath = initialDirectory };
-            var result = dialog.ShowDialog();
-            return result == DialogResult.OK
-                    ? dialog.SelectedPath
-                    : initialDirectory;
+            using (var dialog = new FolderBrowserDialog {Description = $"Locate {type} Source Directory", SelectedPath = initialDirectory})
+            {
+                var result = dialog.ShowDialog();
+                return result == DialogResult.OK ? dialog.SelectedPath : initialDirectory;
+            }
         }
-        
+
         public static string GetFileName(string filter, string initialFile = null)
         {
-            var dialog = new OpenFileDialog { FileName = initialFile, Filter = filter };
-            dialog.ShowDialog();
-            return dialog.FileName != string.Empty ? dialog.FileName : initialFile;
+            using (var dialog = new OpenFileDialog {FileName = initialFile, Filter = filter})
+            {
+                dialog.ShowDialog();
+                return dialog.FileName != string.Empty ? dialog.FileName : initialFile;
+            }
         }
     }
 }
