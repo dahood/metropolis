a2ecd31476e93a826ace1665cb51216fa70ba828
Jonathan McCracken <jon@nexicon.ca>
2016-09-06 20:45:52 -0600

    version

--- src/Metropolis.Api/Properties/AssemblyInfo.cs
+++ src/Metropolis.Api/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.0.0.75")]
-[assembly: AssemblyVersion("0.0.0.75")]
-[assembly: AssemblyFileVersion("0.0.0.75")]
+// [assembly: AssemblyVersion("0.0.0.76")]
+[assembly: AssemblyVersion("0.0.0.76")]
+[assembly: AssemblyFileVersion("0.0.0.76")]
