0f9ddf5e46b1e0fe1e65904bfd8f7d529da79fe1
Jonathan McCracken <jon@nexicon.ca>
2016-10-15 02:34:10 -0600

    got mmvm cross working for wpf...finally

--- src/Metropolis.UI.MVVM.WPF/App.xaml.cs
+++ src/Metropolis.UI.MVVM.WPF/App.xaml.cs
@@ -1,17 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Configuration;
-using System.Data;
-using System.Linq;
-using System.Threading.Tasks;
-using System.Windows;
+﻿using System.Windows;
+using MvvmCross.Core.ViewModels;
+using MvvmCross.Platform;
+using MvvmCross.Wpf.Views;
 
 namespace Metropolis.UI.MVVM.WPF
 {
     /// <summary>
-    /// Interaction logic for App.xaml
+    ///     Bootstrap for MMVM Cross
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            var window = new Container();
+            MainWindow = window;
+
+            DoSetup();
+
+            window.Show();
+        }
+
+        private void DoSetup()
+        {
+            LoadMvxAssemblyResources();
+
+            var presenter = new MvxSimpleWpfViewPresenter(MainWindow);
+
+            var setup = new Setup(Dispatcher, presenter);
+            setup.Initialize();
+
+            var start = Mvx.Resolve<IMvxAppStart>();
+            start.Start();
+        }
+
+        private void LoadMvxAssemblyResources()
+        {
+            for (var i = 0;; i++)
+            {
+                var key = "MvxAssemblyImport" + i;
+                var data = TryFindResource(key);
+                if (data == null)
+                {
+                    return;
+                }
+            }
+        }
     }
 }
--- src/Metropolis.UI.MVVM.WPF/Container.xaml.cs
+++ src/Metropolis.UI.MVVM.WPF/Container.xaml.cs
@@ -3,11 +3,11 @@
 namespace Metropolis.UI.MVVM.WPF
 {
     /// <summary>
-    ///     Interaction logic for MainWindow.xaml
+    ///     Interaction logic for Container.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class Container : Window
     {
-        public MainWindow()
+        public Container()
         {
             InitializeComponent();
         }
