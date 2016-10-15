f12f91db8caac99fd12ab5fe50c307f3cb0e4df9
Jonathan McCracken <jon@nexicon.ca>
2016-10-15 11:51:35 -0600

    adding mac project

---
+++ src/Metropolis.UI.MVVM.Mac/Main.cs
@@ -0,0 +1,13 @@
+﻿using AppKit;
+
+namespace Metropolis.UI.MVVM.Mac
+{
+	static class MainClass
+	{
+		static void Main(string[] args)
+		{
+			NSApplication.Init();
+			NSApplication.Main(args);
+		}
+	}
+}
