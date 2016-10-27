90cbdb72021f1c48c03e75a370998f41b8dd7118
jmccracken <jon@nexicon.ca>
2016-10-26 23:08:43 -0600

    parsing seems to work, now to merge

--- test/Metropolis.Test/Api/Readers/VersionControlReaders/GitLogReaderTest.cs
+++ test/Metropolis.Test/Api/Readers/VersionControlReaders/GitLogReaderTest.cs
@@ -11,29 +11,54 @@ namespace Metropolis.Test.Api.Readers.VersionControlReaders
     [TestFixture]
     public class GitLogReaderTest
     {
+        [SetUp]
+        public void setup()
+        {
+            reader = new GitLogReader();
+
+            result = reader.Parse(new StringReader(oneCommit), twoFileCodebase);
+        }
+
         private readonly string oneCommit =
             "--12ad430--2016-09-30--Jonathan McCracken" + Environment.NewLine +
             "2	1	Metropolis.Api/Domain/CodeBase.cs" + Environment.NewLine +
             "8	0	Metropolis/Views/Canvas.xaml.cs" + Environment.NewLine +
+            "-	-	Metropolis/logo.png" + Environment.NewLine +
             Environment.NewLine;
 
         private readonly CodeBase twoFileCodebase = CodeGraphFixture.Metropolis;
 
+        private GitLogReader reader;
+        private CodeBase result;
+
         [Test]
         public void Should_Parse_Commit_Entry()
         {
-            var reader = new GitLogReader();
-
-            var result = reader.Parse(new StringReader(oneCommit), twoFileCodebase);
-
             result.Commits.Count.Should().Be(1);
             result.Commits[0].CommitHash.Should().Be("12ad430");
-            result.Commits[0].CommitTime.Should().Be(new DateTime(2016,9,30));
+            result.Commits[0].CommitTime.Should().Be(new DateTime(2016, 9, 30));
             result.Commits[0].AuthorName.Should().Be("Jonathan McCracken");
-//
-//            result.AllInstances.Count.Should().Be(2);
-//            result.AllInstances[0].RevisionCount.Should().Be(1);
-//            result.AllInstances[0].RevisionMagnitude.Should().Be(3);
+
+            result.Commits[0].AdditionsAndDeletions[0].AddedLines.Should().Be(2);
+            result.Commits[0].AdditionsAndDeletions[0].DeletedLines.Should().Be(1);
+            result.Commits[0].AdditionsAndDeletions[0].IsBinary.Should().Be(false);
+            result.Commits[0].AdditionsAndDeletions[0].Path.Path.Should().Be("Metropolis.Api/Domain/CodeBase.cs");
+
+            result.Commits[0].AdditionsAndDeletions[1].AddedLines.Should().Be(8);
+            result.Commits[0].AdditionsAndDeletions[1].DeletedLines.Should().Be(0);
+            result.Commits[0].AdditionsAndDeletions[1].IsBinary.Should().Be(false);
+            result.Commits[0].AdditionsAndDeletions[1].Path.Path.Should().Be("Metropolis/Views/Canvas.xaml.cs");
+
+            result.Commits[0].AdditionsAndDeletions[2].AddedLines.Should().Be(0);
+            result.Commits[0].AdditionsAndDeletions[2].DeletedLines.Should().Be(0);
+            result.Commits[0].AdditionsAndDeletions[2].IsBinary.Should().Be(false);
+            result.Commits[0].AdditionsAndDeletions[2].Path.Path.Should().Be("Metropolis/logo.png");
+        }
+
+        [Test]
+        public void Should_Apply_Commit_Against_Instances_In_CodeBase()
+        {
+            //TODO: now need to merge version history into Codebase (if instance exists)
         }
     }
 }
