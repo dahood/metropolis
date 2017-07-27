0ae409d7a5b87bb460f0d22205c54f83e48c27eb
jmccracken <jon@nexicon.ca>
2017-07-27 16:22:09 -0600

    fix for vs 2017

--- src/Metropolis.Api/Properties/AssemblyInfo.cs
+++ src/Metropolis.Api/Properties/AssemblyInfo.cs
@@ -29,6 +29,6 @@ using System.Runtime.InteropServices;
 //
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
-// [assembly: AssemblyVersion("0.0.0.78")]
-[assembly: AssemblyVersion("0.0.0.78")]
-[assembly: AssemblyFileVersion("0.0.0.78")]
+// [assembly: AssemblyVersion("0.0.0.80")]
+[assembly: AssemblyVersion("0.0.0.80")]
+[assembly: AssemblyFileVersion("0.0.0.80")]
--- test/Metropolis.Test/Properties/AssemblyInfo.cs
+++ test/Metropolis.Test/Properties/AssemblyInfo.cs
@@ -5,5 +5,5 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyProduct("Metropolis")]
 [assembly: AssemblyCopyright("Copyright © Jonathan McCracken, Greg Cook, and Richard Hurst 2016")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.0.0.78")]
-[assembly: AssemblyFileVersion("0.0.0.78")]
+[assembly: AssemblyVersion("0.0.0.80")]
+[assembly: AssemblyFileVersion("0.0.0.80")]
