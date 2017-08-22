58351dca2c7810a9c5ee943b57251032f50e8908
Ghassan Karwchan (Contractor) <Ghassan.Karwchan@sjrb.ca>
2017-08-21 21:10:44 -0600

    delete packages folder

--- packages/WpfControls.AutoCompleteTextBox.1.0.0.7/tools/Install.ps1
+++
@@ -1,3 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-$project.Object.References.Add((Join-Path $installPath 'lib\net35\WpfControls.dll'))
--- packages/WpfControls.AutoCompleteTextBox.1.0.0.7/tools/Uninstall.ps1
+++
@@ -1,3 +0,0 @@
-﻿param($installPath, $toolsPath, $package, $project)
-
-$project.Object.References | Where-Object { $_.Name -eq 'WpfControls' } | ForEach-Object { $_.Remove() }
